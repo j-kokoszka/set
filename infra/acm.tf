d2a7dd4c45160d9ca1cb5d2858774ec01ce930d6
Jakub Kokoszka <kuba.kokoszka@gmail.com>
2026-05-16 20:50:43 +0200

    fix: update acm validation to use .name instead of .hostname for cloudflare v5

--- infra/acm.tf
+++ infra/acm.tf
@@ -17,7 +17,7 @@ resource "aws_acm_certificate" "cert" {
 resource "aws_acm_certificate_validation" "cert" {
   provider                = aws.us_east_1
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in cloudflare_dns_record.acm_validation : record.hostname]
+  validation_record_fqdns = [for record in cloudflare_dns_record.acm_validation : record.name]
 }
 
 output "acm_certificate_arn" {
