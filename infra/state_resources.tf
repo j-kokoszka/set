2544669ddcbf791a30c34350be89a4c9fb50d1ac
Jakub Kokoszka <kuba.kokoszka@gmail.com>
2026-05-16 16:05:23 +0200

    chore: expand tfsec suppressions for MVP

--- infra/state_resources.tf
+++ infra/state_resources.tf
@@ -1,6 +1,8 @@
 # S3 Bucket for OpenTofu State
 resource "aws_s3_bucket" "terraform_state" {
   # tfsec:ignore:aws-s3-enable-bucket-logging
+  # tfsec:ignore:aws-s3-encryption-customer-key
+  # tfsec:ignore:aws-s3-specify-public-access-block
   bucket = "${var.project_name}-terraform-state-${var.environment}-77777"
 
   lifecycle {
@@ -37,6 +39,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
 # DynamoDB Table for State Locking
 resource "aws_dynamodb_table" "terraform_locks" {
   # tfsec:ignore:aws-dynamodb-table-customer-key
+  # tfsec:ignore:aws-dynamodb-enable-recovery
   name         = "${var.project_name}-terraform-locks-${var.environment}"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
