995e27470b74c8aadab37530d95226fcd1900920
Jakub Kokoszka <kuba.kokoszka@gmail.com>
2026-05-16 20:45:16 +0200

    fix: restore missing us-east-1 provider for ACM certificate

--- infra/acm.tf
+++ infra/acm.tf
@@ -1,3 +1,9 @@
+# SSL Certificate for CloudFront (Must be in us-east-1)
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
 resource "aws_acm_certificate" "cert" {
   provider          = aws.us_east_1
   domain_name       = var.custom_domain
