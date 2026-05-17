26be2781c481b0a3fab17ef59be8d14b15cb516c
Jakub Kokoszka <kuba.kokoszka@gmail.com>
2026-05-17 11:58:00 +0200

    fix(frontend): split S3 policy statements for least privilege

--- infra/frontend.tf
+++ infra/frontend.tf
@@ -155,14 +155,22 @@ resource "aws_s3_bucket_policy" "frontend_policy" {
         Principal = {
           Service = "cloudfront.amazonaws.com"
         }
-        Action   = [
-          "s3:GetObject",
-          "s3:ListBucket"
-        ]
-        Resource = [
-          aws_s3_bucket.frontend.arn,
-          "${aws_s3_bucket.frontend.arn}/*"
-        ]
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.frontend.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn
+          }
+        }
+      },
+      {
+        Sid       = "AllowCloudFrontServicePrincipalListBucket"
+        Effect    = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:ListBucket"
+        Resource = aws_s3_bucket.frontend.arn
         Condition = {
           StringEquals = {
             "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn
