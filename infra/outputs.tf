3727a262f02dcec28fe5a3695975dd1cfe82f1e1
j-kokoszka <52331756+j-kokoszka@users.noreply.github.com>
2026-05-24 13:58:18 +0200

    fix: force structural recreation of Cognito User Pool to resolve schema mismatch (#132)

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -20,7 +20,7 @@ output "cloudfront_distribution_id" {
 
 output "cognito_user_pool_id" {
   description = "The ID of the Cognito User Pool"
-  value       = aws_cognito_user_pool.user_pool_v2.id
+  value       = aws_cognito_user_pool.user_pool_v3.id
 }
 
 output "cognito_app_client_id" {
